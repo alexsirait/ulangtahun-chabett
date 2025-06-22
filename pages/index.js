7e323028d5ab86ff5e518743d810083894dbbc28
ALEX SIRAIT <alexsirait1001@gmail.com>
2025-06-22 11:31:09 +0700

    PUSH FIRST

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gradient-to-b from-gray-900 via-gray-800 to-gray-900">
+      <p>Selamat Ulang Tahun Chabettkuu</p>
+    </div>
+  );
+}
